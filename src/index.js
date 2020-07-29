5bb4e4d2e60c33715c592b5344925512f67565ad
Dmitriy Mastikov <work.dmitriy34@yandex.ru>
2020-07-29 11:25:56 +0400

    Add uuv4 and return token to Vue

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 const mongoose = require('mongoose')
+
+const { uuid } = require('uuidv4');
+
 const app = express()
 
 const config = require('./config/config')
@@ -20,45 +23,52 @@ mongoose.connection
   .once('open', () => {
     console.log(`Mongoose - successful connection ...`)
     app.listen(process.env.PORT || config.port,
-        () => console.log(`Server start on port ${config.port} ...`))
+      () => console.log(`Server start on port ${config.port} ...`))
   })
   .on('error', error => console.warn(error))
-  
+
 
 
 app.get('/products', (req, res) => {
-    Products.find({}, 'title desc price brand img ', (err, products) => {
-      if (err) {
-        res.sendStatus(500)
-      } else {
-        res.send({ products: products })
-      }
-    }).sort({ _id: -1 })
-  })
+  Products.find({}, 'title desc price brand img ', (err, products) => {
+    if (err) {
+      res.sendStatus(500)
+    } else {
+      res.send({ products: products })
+    }
+  }).sort({ _id: -1 })
+})
 
 app.post('/products', (req, res) => {
-    const product = new Products({
-        title: req.body.title,
-        desc: req.body.description,
-        price: req.body.price,
-        brand: req.body.brand,
-        img: `img/shop/${req.body.brand}/${req.body.img}`
-    })
-    product.save((err, data) => {
-        if (err) {
-            console.log(err)
-        } else {
-            res.send({
-                success: true,
-                message: `Product with ID_${data._id} saved successfully!`
-            })
-        }
-    })
+  const product = new Products({
+    title: req.body.title,
+    desc: req.body.description,
+    price: req.body.price,
+    brand: req.body.brand,
+    img: `img/shop/${req.body.brand}/${req.body.img}`
+  })
+  product.save((err, data) => {
+    if (err) {
+      console.log(err)
+    } else {
+      res.send({
+        success: true,
+        message: `Product with ID_${data._id} saved successfully!`
+      })
+    }
+  })
 })
 
 
 
 
-app.post("/auth", (req, res)=>{
-  console.log(req.body)
+app.post("/auth", (req, res) => {
+  // console.log(req.body)
+
+  const token = uuid();
+  res.send({
+    success: true,
+    token: token
+  })
+
 })
