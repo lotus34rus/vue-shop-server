bc8fcba66fe7a963dd2c2606d03871374c0ae9cc
Dmitriy Mastikov <work.dmitriy34@yandex.ru>
2020-07-29 11:14:54 +0400

    Crere route for auth

--- src/index.js
+++ src/index.js
@@ -13,11 +13,6 @@ app.use(cors())
 
 const Products = require("./models/product-model")
 
-// app.use(require('./routes/products'))
-
-
-
-
 mongoose.Promise = global.Promise
 mongoose.connect(config.dbURL, config.dbOptions)
 
@@ -31,7 +26,6 @@ mongoose.connection
   
 
 
-/*********/
 app.get('/products', (req, res) => {
     Products.find({}, 'title desc price brand img ', (err, products) => {
       if (err) {
@@ -40,44 +34,7 @@ app.get('/products', (req, res) => {
         res.send({ products: products })
       }
     }).sort({ _id: -1 })
-  })  
-/***********/
-// app.get('/products', (req, res) => {
-//     res.send(
-//         [
-
-//             {
-//                 id: 1,
-//                 title: 'Paul Rich v1',
-//                 price: 3200,
-//                 desc: "Fully descriprion for PRv1",
-//                 img: "img/shop/paul-rich/v1.jpg",
-//                 brand: 'Paul Rich'
-//             },
-//             {
-//                 id: 2,
-//                 title: 'Rolex Aquamarine',
-//                 price: 320000,
-//                 desc: "Fully descriprion for Rolex",
-//                 img: "img/shop/rolex/rolex.jpg",
-//                 brand: "Rolex"
-//             },
-
-//             {
-//                 id: 3,
-//                 title: 'Rolex  2',
-//                 price: 3200000,
-//                 desc: "Fully descriprion for Rolex",
-//                 img: "img/shop/rolex/rolex.jpg",
-//                 brand: "Rolex"
-//             }
-
-//         ]
-//     )
-// })
-
-
-/**********/
+  })
 
 app.post('/products', (req, res) => {
     const product = new Products({
@@ -97,4 +54,11 @@ app.post('/products', (req, res) => {
             })
         }
     })
+})
+
+
+
+
+app.post("/auth", (req, res)=>{
+  console.log(req.body)
 })
